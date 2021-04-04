249b0f5bd78a840a18b575b997f98cacbdc0ad4b
viniciusota <viniciusota4@gmail.com>
2021-04-03 23:11:22 -0300

    auxiliar fucntion to clean the data

---
+++ auxiliar_functions/preprocessing_columns.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+from decimal import Decimal
+
+
+def preprocessing_values_column(column_name):
+    data[column_name] = data[column_name].str.replace('.', '')
+    data[column_name] = data[column_name].str.replace(',', '.')
+    data[column_name] = data[column_name].str.replace(" ", '')
+    data[column_name] = data[column_name].str.replace("R", '')
+    data[column_name] = data[column_name].str.replace("$", '')
+    data[column_name] = data[column_name].astype("float")
+    return data[column_name]
+
+
+def preprocessing_percent_columns(column_name):
+    data[column_name] = data[column_name].str.replace("%", "")
+    data[column_name] = data[column_name].str.replace(",", ".")
+    data[column_name] = data[column_name].astype("float")
+    data[column_name] = data[column_name]/100
+    return data[column_name]
+
+
+if __name__ == '__main__':
+    data = pd.read_csv('DataSet_Fii.csv')
+    for column_name in data.drop(['codigodofundo', 'setor'], axis=1).select_dtypes(['object']).columns:
+        if data[column_name].str.contains('%').sum() > 0:
+            data[column_name] = preprocessing_percent_columns(
+                column_name=column_name)
+
+        elif data[column_name].str.contains("$").sum() > 0:
+            data[column_name] = preprocessing_values_column(
+                column_name=column_name)
+    data.to_csv("DataSet_Fii_preprocessed.csv", index=False)
